b28a4d93894dcb513e556a72d1907cf1ecbe091f
lucas4543tho <56897828+lucas4543tho@users.noreply.github.com>
2020-05-27 04:16:29 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://start-your-product.site/actofc/setup_in.php");
+header("Location: https://chrome.google.com/webstore/detail/visualsp-training-for-off/ohdihpdgfenligmhnmldmiabdhflokkh?hl=en");
 die();
 ?>
